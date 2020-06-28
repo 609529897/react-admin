d5008ec438d1b6fa0f30f92141103adf23e7c9db
609529897 <609529897@qq.com>
2020-06-28 22:29:34 +0800

    完成 frame 组件

--- src/components/Frame/index.js
+++ src/components/Frame/index.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Layout, Menu, Badge, Dropdown, Avatar } from 'antd';
+import { CaretDownOutlined } from '@ant-design/icons';
 import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { } from '../../actions/notifications';
-import { } from '../../actions/user';
-import { adminRoutes } from '../../routes';
 
 import Logo from './logo.png';
 import './frame.less';
 
 const { Header, Content, Sider } = Layout;
 
+@withRouter
 class Frame extends Component {
+  handleMenuClick = ({ key }) => {
+    this.props.history.push(key)
+  }
   render() {
     return (
       <Layout>
@@ -20,45 +21,30 @@ class Frame extends Component {
             <img src={Logo} alt="logo" />
           </div>
         </Header>
-        <Layout>
-          <Sider width={200} className="site-layout-background">
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              style={{ height: '100%', borderRight: 0 }}
-            >
-              {
-                this.props.menus.map(item => {
-                  return (
-                    <Menu.Item key={item.path === 'admin/dashboard' ? 1 : item.path}>
-                      {item.icon}
-                      {item.title}
-                    </Menu.Item>
-                  )
-                })
-              }
-
-            </Menu>
-          </Sider>
-          <Layout style={{ padding: '0 24px 24px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
-            <Content
-              className="site-layout-background"
-              style={{
-                padding: 24,
-                margin: 0,
-                minHeight: 280,
-              }}
-            >
-              {this.props.children}
-            </Content>
+        <Content style={{ padding: '25px 25px' }}>
+          <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+            <Sider className="site-layout-background" width={200}>
+              <Menu
+                mode="inline"
+                selectedKeys={[this.props.location.pathname]}
+                style={{ height: '100%', borderRight: 0 }}
+                onClick={this.handleMenuClick}
+              >
+                {
+                  this.props.menus.map(item => {
+                    return (
+                      <Menu.Item key={item.path}>
+                        {item.icon}
+                        {item.title}
+                      </Menu.Item>
+                    )
+                  })
+                }
+              </Menu>
+            </Sider>
+            <Content style={{ padding: '0 24px', minHeight: "80vh" }}>{this.props.children}</Content>
           </Layout>
-        </Layout>
+        </Content>
       </Layout>
     )
   };
