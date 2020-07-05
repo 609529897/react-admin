73eafaad5d9a18aa726dea12e9e15bf88300aa0d
609529897 <609529897@qq.com>
2020-07-05 13:16:39 +0800

    完成文章编辑页

--- src/views/Article/edit.js
+++ src/views/Article/edit.js
@@ -32,47 +32,36 @@ class Edit extends Component {
   constructor() {
     super()
     this.editorRef = createRef()
+    this.formRef = createRef()
     this.state = {
       isLoading: false
     }
   }
-  handleSubmit = (e) => {
-    e.preventDefault()
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        const data = Object.assign({}, values, {
-          createAt: values.createAt.valueOf()
-        })
-        this.setState({
-          isLoading: true
-        })
-        // 在这里可以处理更多想要处理的逻辑
-        saveArticle(this.props.match.params.id, data)
-          .then(resp => {
-            message.success(resp.msg)
-            // 如果需要是要跳转
-            this.props.history.push('/admin/article')
-          })
-          .finally(() => {
-            this.setState({
-              isLoading: false
-            })
-          })
-      }
+  onFinish = (values) => {
+    const data = Object.assign({}, values, {
+      createAt: values.createAt._d
     })
+    this.setState({ isLoading: true })
+    saveArticle(this.props.match.params.id, data)
+      .then(resp => {
+        message.success(resp.msg)
+        this.props.history.push('/admin/article')
+      })
+      .finally(() => {
+        this.setState({ isLoading: false })
+      })
   }
   initEditor = () => {
     this.editor = new E(this.editorRef.current)
     this.editor.customConfig.onchange = (html) => {
-      // html 即变化之后的内容
-      this.props.form.setFieldsValue({
+      this.formRef.current.setFieldsValue({
         content: html
       })
     }
     this.editor.create()
   }
   componentDidMount() {
-    // this.initEditor()
+    this.initEditor()
     this.setState({
       isLoading: true
     })
@@ -80,7 +69,7 @@ class Edit extends Component {
       .then(resp => {
         const { id, ...data } = resp
         data.createAt = moment(data.createAt)
-        this.props.form.setFieldsValue(data)
+        this.formRef.current.setFieldsValue(data)
         this.editor.txt.html(data.content)
       })
       .finally(() => {
@@ -97,40 +86,91 @@ class Edit extends Component {
             {...formItemLayout}
             name="normal_login"
             className="login-form"
-            initialValues={{
-              remember: true,
-            }}
-            onSubmit={this.handleSubmit}
-          // onFinish={onFinish}
+            onFinish={this.onFinish}
+            ref={this.formRef}
           >
+            <br />
             <Form.Item
-              name="username"
+              label="标题"
+              name="title"
+              initialValue="初始值"
               rules={[
                 {
                   required: true,
-                  message: 'Please input your Username!',
-                },
-              ]}
-            >
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                  message: '标题是必填项!',
+                }
+              ]}>
+              <Input placeholder="标题" />
             </Form.Item>
+            <br />
             <Form.Item
-              name="password"
+              label="作者"
+              name="author"
+              initialValue="初始值"
               rules={[
                 {
                   required: true,
-                  message: 'Please input your Password!',
+                  message: '作者是必填项!',
                 },
               ]}
             >
               <Input
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Password"
+                placeholder="作者"
               />
             </Form.Item>
-
-            <Form.Item>
+            <br />
+            <Form.Item
+              label="阅读量"
+              name="amount"
+              initialValue="0"
+              rules={[
+                {
+                  required: true,
+                  message: '阅读量是必填项!',
+                }, {
+                  pattern: /[0-9]$/,
+                  message: '阅读量必须是数值!',
+                }, {
+                  min: 0,
+                  message: '阅读量不可以低于0!',
+                },
+              ]}
+            >
+              <Input
+                placeholder="阅读量"
+              />
+            </Form.Item>
+            <br />
+            <Form.Item
+              label="发布时间"
+              name="createAt"
+              rules={[
+                {
+                  required: true,
+                  message: '时间是必填项!',
+                },
+              ]}
+            >
+              <DatePicker
+                showTime
+                placeholder="选择时间"
+              />
+            </Form.Item>
+            <br />
+            <Form.Item
+              label="内容"
+              name="content"
+              rules={[
+                {
+                  required: true,
+                  message: '内容是必填项!',
+                },
+              ]}
+            >
+            <div className="qua-editor" ref={this.editorRef}></div>
+            </Form.Item>
+            <br />
+            <Form.Item wrapperCol={{ offset: 4 }}>
               <Button type="primary" htmlType="submit" className="login-form-button">
                 保存修改
         </Button>
